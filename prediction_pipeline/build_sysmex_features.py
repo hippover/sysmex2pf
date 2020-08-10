3ca1b0cbf8e7f2c490bb14a21b2d1666a624967e
Hippolyte Verdier <hverdier@pasteur.fr>
2020-08-10 11:37:22 +0200

    Update

--- prediction_pipeline/build_sysmex_features.py
+++ prediction_pipeline/build_sysmex_features.py
@@ -28,6 +28,11 @@ def mode(x):
     q,b = np.histogram(x,bins=15)
     return 0.5*(b[np.argmax(q)] + b[np.argmax(q) + 1])
 
+def entropy(x):
+    p,_ = np.histogram(x,bins=100,density=False)
+    p = p / np.sum(p)
+    return -np.nansum(p*np.log(p))
+
 def kernel_estimates(df,steps):
     """
     :param df: Sysmex measurements (PC 1 and 2)
@@ -58,30 +63,44 @@ def get_features_per_ID(X,mpv,plt_count,pdw,pct,hor_steps,vert_steps):
     high_20_1 = g[1].quantile(0.95)
     low_20_0 = g[0].quantile(0.05)
     low_20_1 = g[1].quantile(0.05)
+    high_50_0 = g[0].quantile(0.98)
+    high_50_1 = g[1].quantile(0.98)
+    low_50_0 = g[0].quantile(0.02)
+    low_50_1 = g[1].quantile(0.02)
     std_0 = g[0].std()
     std_1 = g[1].std()
     mean_0 = g[0].mean()
     mean_1 = g[1].mean()
     mode_0 = g.agg({0: mode})[0]
     mode_1 = g.agg({1: mode})[1]
+    S_0 = g.agg({0:entropy})[0]
+    S_1 = g.agg({1:entropy})[1]
 
-    kde = g.apply(kernel_estimates, (hor_steps, vert_steps))
+    #kde = g.apply(kernel_estimates, (hor_steps, vert_steps))
 
     ratio_10 = (high_10_1 - low_10_1) / (high_10_0 - low_10_0)
     ratio_20 = (high_20_1 - low_20_1) / (high_20_0 - low_20_0)
     mean_std_0 = mean_0 / std_0
     mean_std_1 = mean_1 / std_1
 
+    iqr_0 = g[0].quantile(0.75) - g[0].quantile(0.25)
+    iqr_1 = g[1].quantile(0.75) - g[1].quantile(0.25)
+
+
     corr = g[[0, 1]].corr().loc[(slice(None), 0), 1].droplevel(1)
 
     features = pd.DataFrame(data={"high_10_0": high_10_0, "high_10_1": high_10_1,
                                        "high_20_0": high_20_0, "high_20_1": high_20_1,
+                                       "high_50_0": high_50_0, "high_50_1": high_50_1,
                                        "low_10_0": low_10_0, "low_10_1": low_10_1,
                                        "low_20_0": low_20_0, "low_20_1": low_20_1,
+                                       "low_50_0": low_50_0, "low_50_1": low_50_1,
                                        "mean_0": mean_0, "mean_1": mean_1,
                                        "ratio_10": ratio_10, "ratio_20": ratio_20,
                                        "mean_std_0": mean_std_0, "mean_std_1": mean_std_1,
                                        "mode_0": mode_0, "mode_1": mode_1,
+                                       "S_0": S_0, "S_1": S_1,
+                                       "iqr_0":iqr_0,"iqr_1":iqr_1,
                                        "corr": corr
                                        })
 
@@ -95,7 +114,8 @@ def get_features_per_ID(X,mpv,plt_count,pdw,pct,hor_steps,vert_steps):
         features.loc[features[c] == np.inf, c] = features.loc[features[c] < np.inf, c].max() * 1.5
         features.loc[features[c] == -np.inf, c] = features.loc[features[c] > -np.inf, c].min() * 1.5
 
-    features = pd.merge(features, kde, left_index=True, right_index=True,how="inner")
+    print("Not computing KDE !")
+    #features = pd.merge(features, kde, left_index=True, right_index=True,how="inner")
 
     features = features.dropna(how="any",axis=0)
 
@@ -177,5 +197,8 @@ def build_features(df, sys_phen, train_IDs, hor_steps ,vert_steps, plot=False, s
         plt.tight_layout()
 
     assert features.shape[1] == len(set(features.columns.tolist()))
+
+    features -= features.loc[train_IDs].mean(axis=0)
+    features /= features.loc[train_IDs].std(axis=0)
 
     return features, pd.concat(X_s, axis=0, sort=True)
--- prediction_pipeline/predictors.py
+++ prediction_pipeline/predictors.py
@@ -6,6 +6,7 @@ from sklearn.decomposition import PCA
 from scipy.special import comb
 from scipy.stats import pearsonr
 from tqdm.notebook import tqdm
+from sklearn.ensemble import GradientBoostingRegressor
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -25,71 +26,57 @@ class predictor():
 
         n_iter = 3
 
-        scores = -np.inf*np.ones((len(self.n_comps_pls),len(self.n_comps_pca),len(self.adjust_y)))
-        scores_lm = -np.inf*np.ones((len(self.n_comps_pls),len(self.n_comps_pca),len(self.adjust_y)))
-        BIC_pls = np.inf*np.ones((len(self.n_comps_pls), len(self.n_comps_pca), len(self.adjust_y)))
-        BIC_lm = np.inf*np.ones((len(self.n_comps_pls), len(self.n_comps_pca), len(self.adjust_y)))
+        scores = -np.inf*np.ones((len(self.n_comps_pca),len(self.adjust_y)))
+        scores_lm = -np.inf*np.ones((len(self.n_comps_pca),len(self.adjust_y)))
+        BIC_pls = np.inf*np.ones((len(self.n_comps_pca), len(self.adjust_y)))
+        BIC_lm = np.inf*np.ones((len(self.n_comps_pca), len(self.adjust_y)))
 
         progress_bar = tqdm(total=len(self.n_comps_pls)*len(self.n_comps_pca)*len(self.adjust_y))
-        for i in range(len(self.n_comps_pls)):
-            for j in range(len(self.n_comps_pca)):
-                for k in range(len(self.adjust_y)):
+        for j in range(len(self.n_comps_pca)):
+            for k in range(len(self.adjust_y)):
 
-                    if self.choice_criterion == "R2":
-                        score = np.zeros(n_iter)
-                        score_lm = np.zeros(n_iter)
+                if self.choice_criterion == "R2":
+                    score_lm = np.zeros(n_iter)
 
-                        for iter_ in range(n_iter):
-
-                            X_true = features.copy()
-                            Y_true = Y_.loc[X_true.index].copy()
-
-                            preds, preds_lm, _ = self.predict_in_splits(X_true,Y_true,sys_phen,
-                                                                     self.adjust_y[k],
-                                                                     self.n_comps_pls[i],
-                                                                     self.n_comps_pca[j])
-
-                            score[iter_] = r2_score(Y_true,preds)
-                            score_lm[iter_] = r2_score(Y_true,preds_lm)
-
-                        if self.n_comps_pls[i] >= len(self.good_columns):
-                            scores[i, j, k] = np.mean(score)
-                        scores_lm[i, j, k] = np.mean(score_lm)
-                    elif self.choice_criterion == "BIC":
-                        # To compute the BIC, we train on the whole dataset
+                    for iter_ in range(n_iter):
 
                         X_true = features.copy()
                         Y_true = Y_.loc[X_true.index].copy()
 
-                        self.train(X_true,Y_true,sys_phen,
-                                   self.adjust_y[k],
-                                   self.n_comps_pls[i],
-                                   self.n_comps_pca[j])
-                        preds_pls, preds_lm, _ = self.predict(X_true,sys_phen,self.adjust_y[k])
-                        error_2_pls = np.std(Y_true - preds_pls)**2
-                        error_2_lm = np.std(Y_true - preds_lm)**2
+                        preds_lm, pred_sys = self.predict_in_splits(X_true,Y_true,sys_phen,
+                                                                 self.adjust_y[k],
+                                                                 self.n_comps_pca[j])
 
-                        scores[i, j, k] = r2_score(Y_true,preds_pls)
-                        scores_lm[i, j, k] = r2_score(Y_true, preds_lm)
+                        score_lm[iter_] = r2_score(Y_true-pred_sys,preds_lm)
 
-                        n_params = 0
-                        # Adjust y
-                        if self.adjust_y[k] == True:
-                            n_params += 4
-                        # PCA, we finally don't count it as it hasn't to do with Y
-                        # Last term because principal components are orthogonal
-                        # n_params += X_true.shape[1]*self.n_comps_pca[j] - comb(self.n_comps_pca[j],2)
-                        # adjust loadings, we don't count them either
-                        # n_params += 4*self.n_comps_pca[j]
-                        # PLS
-                        n_params_pls = n_params + len(self.good_columns)*self.n_comps_pls[i] + self.n_comps_pls[i] - comb(self.n_comps_pls[i],2)
-                        # Only LM
-                        n_params_lm = n_params + self.n_comps_pca[j] * self.n_comps_pls[i] + self.n_comps_pls[i]
-                        if self.n_comps_pls[i] <= len(self.good_columns):
-                            BIC_pls[i,j,k] = preds_pls.shape[0]*np.log(np.mean(error_2_pls)) + n_params_pls*np.log(preds_pls.shape[0])
-                        BIC_lm[i,j,k] = preds_lm.shape[0]*np.log(np.mean(error_2_lm)) + n_params_lm*np.log(preds_lm.shape[0])
+                    scores_lm[j, k] = np.mean(score_lm)
+                elif self.choice_criterion == "BIC":
+                    # To compute the BIC, we train on the whole dataset
 
-                    progress_bar.update(1)
+                    X_true = features.copy()
+                    Y_true = Y_.loc[X_true.index].copy()
+
+                    self.train(X_true,Y_true,sys_phen,
+                               self.adjust_y[k],
+                               self.n_comps_pca[j])
+                    preds_lm, preds_sys = self.predict(X_true,sys_phen,self.adjust_y[k])
+                    error_2_lm = np.std((Y_true - preds_sys) - (preds_lm - preds_sys))**2
+                    scores_lm[j, k] = r2_score(Y_true - preds_sys, preds_lm - preds_sys)
+
+                    n_params = 0
+                    # Adjust y
+                    # if self.adjust_y[k] == True:
+                    #    n_params += 4
+                    # PCA, we finally don't count it as it hasn't to do with Y
+                    # Last term because principal components are orthogonal
+                    # n_params += X_true.shape[1]*self.n_comps_pca[j] - comb(self.n_comps_pca[j],2)
+                    # adjust loadings, we don't count them either
+                    # n_params += 4*self.n_comps_pca[j]
+                    # Only LM
+                    n_params_lm = 1 + self.n_comps_pca[j]*(features.shape[1] + 1)
+                    BIC_lm[j,k] = preds_lm.shape[0]*np.log(np.mean(error_2_lm)) + n_params_lm*np.log(preds_lm.shape[0])
+
+                progress_bar.update(1)
 
         progress_bar.close()
 
@@ -111,23 +98,18 @@ class predictor():
         else:
             raise Exception("choice_critetrion should be BIC or R2")
 
-        self.best_n_comps_pls = self.n_comps_pls[best_params[0]]
-        self.best_n_comps_pca = self.n_comps_pca[best_params[1]]
-        self.best_adjust_y = self.adjust_y[best_params[2]]
-        self.best_score = scores[best_params[0],best_params[1],best_params[2]]
+        self.best_n_comps_pca = self.n_comps_pca[best_params[0]]
+        self.best_adjust_y = self.adjust_y[best_params[1]]
+        self.best_score = scores[best_params[0],best_params[1]]
         self.best_params_found = True
 
         print("==== %s ====" % self.pheno_name)
         print("Chosen with %s" % self.choice_criterion)
-        print("Obtained R^2 of %.3f" % self.best_score)
-        print("loadings only = %s " % self.best_only_loadings)
+        print("Obtained R^2 of %.2f on residuals" % self.best_score)
         print("n_comps_pca = %d " % self.best_n_comps_pca)
-        print("n_comps_pls = %d " % self.best_n_comps_pls)
-        print("adjust y = %s" % self.best_adjust_y)
 
-    def predict_in_splits(self,X_true,Y_true,sys_phen,adjust_y,n_comps_pls,n_comps_pca):
+    def predict_in_splits(self,X_true,Y_true,sys_phen,adjust_y,n_comps_pca):
 
-        preds = pd.Series(data=np.zeros(X_true.shape[0]),index=X_true.index)
         preds_lm = pd.Series(data=np.zeros(X_true.shape[0]),index=X_true.index)
         preds_sysmex = pd.Series(data=np.zeros(X_true.shape[0]), index=X_true.index)
         skf = StratifiedKFold(n_splits=12)
@@ -146,17 +128,17 @@ class predictor():
                        Y_.loc[index_train],
                        sys_phen.loc[index_train],
                        adjust_y,
-                       n_comps_pls,
                        n_comps_pca)
-            preds.loc[index_test], preds_lm.loc[index_test], preds_sysmex.loc[index_test] = self.predict(X_test,sys_phen.loc[index_test],adjust_y)
+            preds_lm.loc[index_test], preds_sysmex.loc[index_test] = self.predict(X_test,sys_phen.loc[index_test],adjust_y)
 
-        return preds, preds_lm, preds_sysmex
+        return preds_lm, preds_sysmex
 
-    def train(self,X_t,Y_t,sys_phen,adjust_y,n_comps_pls,n_comps_pca):
+    def train(self,X_t,Y_t,sys_phen,adjust_y,n_comps_pca):
 
         X_ = X_t.copy()
         Y_ = Y_t.copy()
 
+        # Adjust phenotypes
         self.lm_y = LinearRegression().fit(sys_phen[["PLT_count","MPV","PDW","PCT"]],Y_)
         if adjust_y:
             sys_pred = self.lm_y.predict(sys_phen[["PLT_count","MPV","PDW","PCT"]])
@@ -165,29 +147,29 @@ class predictor():
         Y_ = Y_-sys_pred
 
         self.pca = PCA(n_components=n_comps_pca)
-        #self.pca = PCA(n_components="mle")
-        self.pca.fit(X_)
-        #print("MLE n_components is %d" % self.pca.n_components_)
 
+        # Option 1 : ajuster les loadings
+        """
+        self.pca.fit(X_)
         X_load = pd.DataFrame(data=self.pca.transform(X_),index=X_.index)
         self.lr = LinearRegression().fit(sys_phen[["PLT_count","MPV","PDW","PCT"]],X_load)
         X_load = X_load - self.lr.predict(sys_phen.loc[X_.index,["PLT_count","MPV","PDW","PCT"]])
+        """
 
-        X_ = pd.DataFrame(data=self.pca.inverse_transform(X_load),index=X_.index)
-        self.good_columns = np.where(np.std(X_,axis=0) > 0.001)[0]
+        #Option 2: ajuster les features
+        self.lr = LinearRegression().fit(sys_phen[["PLT_count","MPV","PDW","PCT"]],X_)
+        X_ = X_ - self.lr.predict(sys_phen.loc[X_.index,["PLT_count","MPV","PDW","PCT"]])
+        #X_load = pd.DataFrame(data=self.pca.fit_transform(X_),index=X_.index)
+        #
+        #self.lm = LinearRegression().fit(X_load,Y_)
+        self.lm = PLSRegression(n_components=n_comps_pca).fit(X_,Y_)
 
-        #print(n_comps_pca,X_.shape[1],len(self.good_columns))
-
-        self.pls = PLSRegression(n_components=min(n_comps_pls,len(self.good_columns))).fit(X_.iloc[:,self.good_columns],Y_)
-
-        self.lm = LinearRegression().fit(X_load,Y_)
         self.best_params_set = False
 
     def train_on_best_params(self,X_t,Y_t,sys_phen):
         assert self.best_params_found == True
         self.train(X_t,Y_t,
                 sys_phen,self.best_adjust_y,
-                self.best_n_comps_pls,
                 self.best_n_comps_pca)
         self.best_params_set = True
 
@@ -197,27 +179,29 @@ class predictor():
         else:
             sys_pred = np.zeros(X_.shape[0])
 
+        # Option 1
+        """
         X_load = pd.DataFrame(data=self.pca.transform(X_),index=X_.index)
         X_load = X_load - self.lr.predict(sys_phen.loc[X_.index,["PLT_count","MPV","PDW","PCT"]])
-
         X_ = pd.DataFrame(data=self.pca.inverse_transform(X_load),index=X_.index)
+        """
+        # Option 2
+        X_ = X_ - self.lr.predict(sys_phen.loc[X_.index,["PLT_count","MPV","PDW","PCT"]])
+        #X_load = pd.DataFrame(data=self.pca.transform(X_),index=X_.index)
+        #preds_lm = pd.Series(data=self.lm.predict(X_load) + sys_pred,index=X_.index)
+        preds_lm = pd.Series(data=self.lm.predict(X_)[:,0] + sys_pred, index=X_.index)
 
-        preds = pd.Series(data=self.pls.predict(X_.iloc[:,self.good_columns])[:,0] + sys_pred,index=X_.index)
-        preds_lm = pd.Series(data=self.lm.predict(X_load) + sys_pred,index=X_.index)
-
-        return preds, preds_lm, sys_pred
+        return preds_lm, sys_pred
 
     def predict_with_best(self,X_,sys_phen, return_sysmex = False):
         assert self.best_params_found == True
         assert self.best_params_set == True
-        pred_pls, pred_loading, pred_sysmex = self.predict(X_,sys_phen,
+        pred_loading, pred_sysmex = self.predict(X_,sys_phen,
                                               self.best_adjust_y)
         if return_sysmex:
             return pred_sysmex
-        elif self.best_only_loadings:
-            return pred_loading
         else:
-            return pred_pls
+            return pred_loading
 
     def predict_in_splits_with_best(self,X_true,Y_true,sys_phen):
         assert self.best_params_found == True
@@ -226,7 +210,6 @@ class predictor():
                                       Y_true,
                                       sys_phen,
                                       self.best_adjust_y,
-                                      self.best_n_comps_pls,
                                       self.best_n_comps_pca)
         self.best_params_set = True
         # Because predict_in_splits implies training,
@@ -246,6 +229,5 @@ class KDE_predictor(predictor):
 class agg_features_predictor(predictor):
     def __init__(self,pheno_name="pheno_name",criterion="R2"):
         super().__init__(pheno_name+"_agg",criterion)
-        self.n_comps_pls = [1,2,3]
-        self.n_comps_pca = [1,2,3,4]
+        self.n_comps_pca = [1,2,3,4,5]
         self.adjust_y = [True]
